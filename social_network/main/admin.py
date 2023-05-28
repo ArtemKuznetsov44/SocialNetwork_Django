ea8936bd526e0cc8c8ae8c76103737a5bfa06d32
ArtemKuznetsov44 <kuzia.artem456485@mail.ru>
2023-05-28 22:47:14 +0300

    Artem-task social-network-5

--- social_network/main/admin.py
+++ social_network/main/admin.py
@@ -5,3 +5,14 @@ from main.models import *
 admin.site.register(Setting)
 admin.site.register(Gender)
 admin.site.register(User)
+admin.site.register(ContentType)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin): 
+    list_display = ['id', 'content_type', 'content', 'text', 'like_status', 'comment_status', 'created_at']
+    list_editable = ['like_status', 'comment_status']
+    list_filter = ['id', 'created_at']
+    search_fields = ['text']
+
+    
